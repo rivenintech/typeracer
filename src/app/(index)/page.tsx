1013ca2de9932ac8160e83dcd3389a9fa9433926
rivenintech <contact@rivenintech.com>
2026-03-06 15:48:13 +0100

    feat: implement login with supabase auth

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,8 +1,17 @@
+import { createClient } from "@/lib/supabase/server";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { PlayersTable } from "./components/PlayersTable";
 import { TypingArea } from "./components/TypingArea";
 
 export default async function Home() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getClaims();
+
+  if (error || !data?.claims) {
+    redirect("/auth/login");
+  }
+
   const cookieStore = await cookies();
   const username = cookieStore.get("username")?.value || "Anonymous";
 
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,25 +1,19 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { updateSession } from "@/lib/supabase/proxy";
+import { type NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  const username = request.cookies.get("username");
-
-  if (!username) {
-    return NextResponse.redirect(new URL("/enter-username", request.url));
-  }
+export async function proxy(request: NextRequest) {
+  return await updateSession(request);
 }
 
 export const config = {
   matcher: [
     /*
      * Match all request paths except:
-     * - /enter-username (username entry page)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
      */
-    "/((?!enter-username|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
